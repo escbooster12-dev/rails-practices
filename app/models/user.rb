4fd5b5534c707c6f8b098cd8fbced9abd9700ded
Joshua Escarilla <joshua.escarilla@sun-asterisk.com>
2020-11-24 16:03:46 +0800

    add model observer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :username, presence: true,
                        length: { minimum: 3, maximum: 25 },
                        uniqueness: { case_sensitive: false }
