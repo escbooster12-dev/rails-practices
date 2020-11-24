d8a5893c5626dce9d416bf6e27fd1853e2a24069
Joshua Escarilla <joshua.escarilla@sun-asterisk.com>
2020-11-24 11:45:51 +0800

    Add users table and users model

--- db/migrate/20201119051400_create_articles.rb
+++ db/migrate/20201119051400_create_articles.rb
@@ -3,7 +3,6 @@ class CreateArticles < ActiveRecord::Migration[6.0]
     create_table :articles do |t|
       t.string :title
       t.text :description
-      t.int :user_id
       t.timestamps
     end
   end
