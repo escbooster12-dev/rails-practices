9e9f788e6ab23f95f02ad1772956f4ccba472d1c
Joshua Escarilla <joshua.escarilla@sun-asterisk.com>
2020-11-24 11:24:04 +0800

    create articles CRUD

---
+++ db/migrate/20201119051400_create_articles.rb
@@ -0,0 +1,10 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+      t.int :user_id
+      t.timestamps
+    end
+  end
+end
