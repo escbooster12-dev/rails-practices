57b99440ee2d90a30f85727f94d035da1364d2d7
Joshua Escarilla <joshua.escarilla@sun-asterisk.com>
2020-11-24 13:55:08 +0800

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-    has_many :articles
+  validates :username, presence: true,
+                       length: { minimum: 3, maximum: 25 },
+                       uniqueness: { case_sensitive: false }
+  EMAIL_REGEX = VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true,
+                    length: { maximum: 105 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: EMAIL_REGEX }
+  has_many :articles
 end
