d85a0dc0dc03030c9364a2716f5d15d05b160097
Joshua Escarilla <joshua.escarilla@sun-asterisk.com>
2020-11-25 13:10:45 +0800

    add more authentication methods in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,15 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    unless logged_user?
+      flash[:error] = 'You must be logged to perform that action'
+      redirect_to login_path
+    end
+  end
 end
